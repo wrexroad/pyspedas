5095606757ca8354a7dd1b112e02481da91ed398
Warren Rexroad <wrexroad@ssl.berkeley.edu>
2024-05-09 19:51:43 +0000

    added barrel_sp_fold_4

--- pyspedas/barrel/SpecData.py
+++ pyspedas/barrel/SpecData.py
@@ -1106,8 +1106,183 @@ class SpecData:
 
     return [params, param_ranges, modvals, secondmodvals, chisquare, dof]
 
-  def _barrel_sp_fold_m4(self):
-    return
+  def _barrel_sp_fold_m4(self, phmean, phwidth, ctwidth, usebins, maxcycles):
+    subspec = self.subspec
+    subspecerr = self.subspecerr
+    model = self.model
+    drm = self.drm
+    drm2 = self.drm2
+
+    if (model == 1):
+      #This formula for approximate e-folding from a count ratio between
+      #two bands will be empirical from simulations.  For now, we will start
+      #with folding energy = 300 keV
+      #    countratio = total(subspec[where(ebins GT ?? and ebins LT ??)]) / $  
+      #                 total(subspec[where(ebins GT ?? and ebins LT ??)]) 
+      #    startpar = ?? ;function of countratio
+      startpar = 300.
+      tryspec = np.matmul(drm, np.exp(-phmean/startpar)*phwidth)
+    elif (model == 2):
+        startpar = 1000.
+        tryspec = phmean*0.
+        tryspec[
+          (
+            np.where( np.abs(phmean-startpar) == np.min(np.abs(phmean-startpar)) )[0]
+          )[0]
+        ] = 1.
+        tryspec = np.matmul(drm, tryspec*phwidth)
+    else:
+      print('Only exponential or monoenergetic spectrum is currently supported.')
+
+    #Find a starting normalization by scaling area of model and data
+    #(this will be the same procedure for every starting model):
+    startnorm = np.sum( subspec[usebins]*ctwidth[usebins] ) / np.sum( tryspec[usebins]*ctwidth[usebins] )
+
+    #Try a starting range around these trial values.  If the minimum 
+    #chi-square is not on the boundary, zoom in.  If it is, zoom out.
+    #In either case, recenter.
+
+    points = 10   #always run a 21x21(x21) grid
+    scaling = np.array([0.5,0.5,0.5])  #[norm,par,drm]: best values +/- 50% (norm&par) or just +/- 0.5 (drm)
+
+    print(
+      'iter#', 'startpar','startnorm','startdrm','bestpar','bestnorm','bestdrm',
+      'scalepar','scalenorm','scaledrm','bestchi'
+    )
+    #format='(a8,6a11,3a13,a10)'
+
+    #Iterate the fit, adjusting the scale dynamically:
+    for i in range(maxcycles):
+        [bestpar, bestnorm, bestdrm, bestparn, bestnormn, bestdrmn, modvals, chiarray, bestchi, pararray, normarray, drmarray] = self._barrel_sp_fitgrid4(
+          self, phmean, phwidth, usebins, startpar, startnorm, startdrm, points, scaling
+        )
+        #if best value is not on boundary, zoom in or finish.
+        #Note that zooming in or out on scalingdrm doesn't do anything if
+        #you aren't using two drms.
+
+        if (np.abs(bestnormn) != points and scaling[0] >= 0.001):
+          scaling[0] /= 2.5
+        if (np.abs(bestparn) !=  points and scaling[1] >= 0.001):
+          scaling[1] /= 2.5
+        if (np.abs(bestdrmn) !=  points and scaling[2] >= 0.001):
+          scaling[2] /= 2.5
+
+        #If scaling is now very fine, break.  Note that the last values of the
+        #scaling parameters recorded here aren't really the last
+        #values used, the last value used could be 2.5 times higher in one or more:
+        if (scaling[0] < 0.001 and scaling[1] < 0.001 and scaling[2] < 0.001):
+          break
+
+        if (np.abs(bestnormn) == points):
+          scaling[0] *= 2.0
+        if (np.abs(bestparn) == points):
+          scaling[1] *= 2.0
+        if (np.abs(bestdrmn) == points):
+          scaling[2] *= 2.0
+
+        print(i,startpar,startnorm,startdrm,bestpar,bestnorm,bestdrm,scaling[1],scaling[0],scaling[2],bestchi)
+        #format='(i8,6f11.3,3f13.6,f13.4)'
+
+        startpar = bestpar
+        startnorm = bestnorm
+        startdrm = bestdrm
+
+    #If it never got to the finest scale, break with error:
+    if (scaling[0] > 0.001 or scaling[1] > 0.001 or scaling[2] > 0.001):
+      print('Fit failed to converge in maximum number of cycles.')
+
+    #Set most output variables (either 2 or 3 best-fit params depending on
+    #treatment of response matrices:
+    params = [bestnorm, bestpar, bestdrm]
+    chisquare = bestchi
+    dof = usebins.size - 2
+
+    #Only one thing left: the error on the parameters.  This requires more
+    #effort.  Here we will wander radially outwards until we find that the
+    #whole boundary has chisq > chimin
+    #Always center on the best value:
+    startpar = bestpar
+    startnorm = bestnorm
+    stardrm = bestdrm
+    points = 10
+
+    #Create masks for the outer boundary of the chi-square space:
+    edges1 = np.zeros([2*points+1,2*points+1,2*points+1], dtype=int)
+    edges2 = np.zeros([2*points+1,2*points+1,2*points+1], dtype=int)
+    edges3 = np.zeros([2*points+1,2*points+1,2*points+1], dtype=int)
+    
+    edges1[:,:,0] = 1
+    edges1[:,:,2*points] = 1
+    edges2[:,0,:] = 1
+    edges2[:,2*points,:] = 1
+    edges3[0,:,:] = 1
+    edges3[2*points,:,:] = 1
+
+    #Create initial values for error bar search:
+    scaling = [0.1, 0.1, 0.1]   #first guess
+    scaling0 = scaling
+    minscaling = scaling
+    goingup = [0,0,0]
+
+    for i in maxcycles:
+      [bestpar, bestnorm, bestdrm, bestparn, bestnormn, bestdrmn, modvals, chiarray, bestchi, pararray, normarray, drmarray] = self._barrel_sp_fitgrid4(
+        self, phmean, phwidth, usebins, startpar, startnorm, startdrm, points, scaling
+      )
+      
+      #First see if the contour is completely closed:
+      #Look for chisq < min_chisq + 1 on boundary:
+      w1 = np.where(edges1.astype(bool) and (chiarray <= chisquare + 1.))[0]
+      nw1 = w1.size
+      w2 = np.where(edges2.astype(bool) and (chiarray <= chisquare + 1.))[0]
+      nw2 = w2.size
+      w3 = np.where(edges3.astype(bool) and (chiarray <= chisquare + 1.))[0]
+      nw3 = w3.size
+      nw=[nw1,nw2,nw3]
+
+      #If the boundary is entirely outside of the chi-square contour, zoom
+      #in ALL AXES by a factor of 2, unless you had already zoomed out, in which 
+      #case you've actually identified the right scale:
+      if (np.sim(nw) == 0):
+          if (np.sum(goingup) == 3):
+            break
+          for j in (0, 1, 2):
+            if (not goingup[j]):
+              scaling[j] /= 2.0
+          continue
+      
+      #If boundary not entirely clear, take each axis separately, and
+      #expand or contract the scaling:
+      for j in (0, 1, 2):
+        if (nw[j] > 0):
+          goingup[j] = 1
+          scaling[j] *= 2.0
+        else:
+          if (not goingup[j]):
+            scaling[j] /= 2.0
+
+    #Now that we've found the appropriate scaling (within a factor
+    #of 2 of the point where the last good fit appears on the boundary), 
+    #do one very fine map of chisquare space to find the error bars:
+
+    points = 40
+    [bestpar, bestnorm, bestdrm, bestparn, bestnormn, bestdrmn, modvals, chiarray, bestchi, pararray, normarray, drmarray] = self._barrel_sp_fitgrid4(
+      self, phmean, phwidth, usebins, startpar, startnorm, startdrm, points, scaling
+    )
+
+    #Pick out the subset of points within the min(chisquare)+1. contour:
+    w = np.where(chiarray < chisquare + 1.)[0]
+    nw = w.size
+    if (nw == 0):
+      print('Failure in finding error bars.')
+
+    #This makes up the last needed output parameter: ranges of the parameters
+    param_ranges = [
+      [np.min(normarray[w]), np.max(normarray[w])],
+      [np.min(pararray[w]), np.max(pararray[w])],
+      [np.min(drmarray[w]), np.max(drmarray[w])]
+    ]
+
+    return [params, param_ranges, modvals, chisquare, dof]
   
   def _barrel_sp_fitgrid1(self, phmean, phwidth, usebins, startpar, startnorm, points, scaling):
     subspec = self.subspec
@@ -1324,10 +1499,10 @@ class SpecData:
       drmvector = drmmin + (drmmax-drmmin)*np.arange(pts)/(1.*(pts-1.))
 
     #Set up the output arrays:
-    pararray = np.zeros([pts,pts,pts])
+    pararray  = np.zeros([pts,pts,pts])
     normarray = np.zeros([pts,pts,pts])
-    drmarray = np.zeros([pts,pts,pts])
-    chiarray = np.zeros([pts,pts,pts])
+    drmarray  = np.zeros([pts,pts,pts])
+    chiarray  = np.zeros([pts,pts,pts])
 
     #Initialize best chi-square as something awful:
     bestchi = 1.e10
