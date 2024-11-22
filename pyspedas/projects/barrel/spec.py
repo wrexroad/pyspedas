ad397e9f135a362d66a143b5674337a5547ae931
Warren Rexroad <wrexroad@ssl.berkeley.edu>
2024-11-22 21:59:58 +0000

    added BARREL spectroscopy code for creating specstruct and processing spectra

--- pyspedas/projects/barrel/spec.py
+++ pyspedas/projects/barrel/spec.py
@@ -7,15 +7,14 @@ from .dbase import dbase
 class spec:
 
   @staticmethod
-  def create_timestamp_mask(ts, start, end):
+  def create_timestamp_mask(ts, start, end, dur=0):
     #convert start and end times to seconds since epoch
     epoch = datetime(1970, 1, 1)
-    start_seconds = (datetime.strptime(start, "%Y-%m-%d/%H:%M") - epoch).total_seconds()
-    end_seconds = (datetime.strptime(end, "%Y-%m-%d/%H:%M") - epoch).total_seconds()
+    start_seconds = (datetime.strptime(start, "%Y-%m-%d/%H:%M:%S") - epoch).total_seconds()
+    end_seconds = (datetime.strptime(end, "%Y-%m-%d/%H:%M:%S") - epoch).total_seconds()
 
     #add a section of valid timestamps to the boolean mask
-    return ((ts >= start_seconds) & (ts <= end_seconds))
-
+    return ((ts >= start_seconds) & (ts+dur <= end_seconds))
 
   @staticmethod
   def ave_spec(ts, cnts, periods):
@@ -66,7 +65,7 @@ class spec:
       'size': size,                                         # number of bins in spectrum 
       'altitude': -1,                                       # altitude in km
       'maglat': -1,                                         # magnetic latitude in degrees
-      'bkg_method': -1,                                     # 1 = from data stream, 2 =from model
+      'bkg_method': 1,                                      # 1 = from data stream, 2 =from model
       'src_spec': np.ones(size, dtype="float") * -1,        # summed source spectrum, deadtime corrected       
       'src_spec_err': np.ones(size, dtype="float") * -1,    # error in summed source spectrum
       'bkg_spec': np.ones(size, dtype="float") * -1,        # summed background spectrum, deadtime corrected        
@@ -159,41 +158,121 @@ class spec:
 
     return [width, mean, gmean]
   
-  
   @staticmethod
-  def barrel_sp_collect_spectra(ss, altitude=None, maglat=None, bkg_method=1):
+  def barrel_sp_collect_spectra(ss, altitude=None, maglat=None):
     if altitude is None:
       pyspedas.barrel.ephm(ss["trange"][0], ss["payload"])
       ts, alt = pytplot.get_data('brl'+ss["payload"]+'_GPS_Alt')
-      ss["altitude"] = altitude if altitude or alt[int(alt.size/2)]
+      ss["altitude"] = altitude if altitude else alt[int(alt.size/2)]
       print(ss["altitude"])
 
-    if maglat is None and bkg_method==2:
+    if maglat is None and ss["bkg_method"]==2:
       print("Please supply magnetic latitude")
       return
     
     width, mean, gmean = spec.edge_products(ss["e_bins"]) 
     
-    for (start, end) in ss["event_periods"]:
+    for (start, end) in ss["trange"]:
       (spectrum, livetime, rawtime) = spec.barrel_sp_collect_one_spectrum(ss, start, end)
+      if livetime == -1 and rawtime == 1:
+        #this is the first run, overwrite the fill values
+        ss["src_spec"] = spectrum
+        ss["src_live"] = livetime
+        ss["src_time"] = rawtime
+      else:
+        ss["src_spec"] += spectrum
+        ss["src_live"] += livetime
+        ss["src_time"] += rawtime
+      
+      #Revert to cts/bin instead of cts/keV to calculate errors:
+      raw = ss["src_spec"]*(width*ss["src_live"]/ss["src_time"])
+      #Average of upper and lower limits from Gehrels 1986:
+      ss["src_spec_err"] = ( np.sqrt(raw-0.25) + np.sqrt(raw+0.75) + 1. ) / 2.
+      #Return to /keV for the error:
+      ss["src_spec_err"] /= (width*ss["src_live"]/ss["src_time"])
   
+    if ss["bkg_method"] == 1:
+      for (start, end) in ss["bkgtrange"]:
+        (spectrum, livetime, rawtime) = spec.barrel_sp_collect_one_spectrum(ss, start, end)
+        if livetime == -1 and rawtime == 1:
+          #this is the first run, overwrite the fill values
+          ss["bkg_spec"] = spectrum
+          ss["bkg_live"] = livetime
+          ss["bkg_time"] = rawtime
+        else:
+          ss["bkg_spec"] += spectrum
+          ss["bkg_live"] += livetime
+          ss["bkg_time"] += rawtime
+      
+      #Revert to cts/bin instead of cts/keV to calculate errors:
+      raw = ss["bkg_spec"]*(width*ss["bkg_live"]/ss["bkg_time"])
+      #Average of upper and lower limits from Gehrels 1986:
+      ss["bkg_spec_err"] = (np.sqrt(raw-0.25) + np.sqrt(raw+0.75) + 1. ) / 2.
+      #Return to /keV for the error:
+      ss["bkg_spec_err"] /= (width*ss["bkg_live"]/ss["bkg_time"])
+    #else:
+      #***STILL NEED TO FINISH PORTING BACKGROUND METHOD 2**
+      ##Generate background model in 5 keV bins and rebin:
+      #fine_ebins = findgen(1400)*5.+20.
+      #edge_products, fine_ebins, mean=fmean, width=fwidth
+      #bkgfine = barrel_make_model_bkg( fmean, ss.altitude, ss.maglat )
+      #bkgnormal = brl_rebin(bkgfine,fine_ebins,ss.ebins,flux=1)
+      #ss["bkg_spec"] = bkgnormal
+      #ss["bkg_live"] = 1.
+      #ss["bkg_time"] = 1.
+      ##Take somewhat arbitrary 5% error bars, even though not really uncorrelated:
+      #ss["bkg_spec_err"] = 0.05*ss["bkg_spec"]
+
   @staticmethod
   def barrel_sp_collect_one_spectrum(ss, start, end):
-    if ss["is_slow"]:
-      datstr = "SSPC"
-      dur = 32
-    else:
-      datstr="MSPC"
-      dur=4
+    dur = 32 if ss["is_slow"] else 4
 
-    ebins=spec.make_standard_energies(True)
-    sspc_ts, sspc_cnts, energy_levels = pytplot.get_data(ss["tplot_var"])
-    spectots = raw_sspc.sum(1)
+    spect = pytplot.get_data(ss["tplot_var"])
+    spectots = spect.y.sum(1)
     goodspect = spectots >= 0.1
 
-    pyspedas.barrel.rcnt(start, end, ss["payload"])
-    rcnt_ts, rcnt_cnts = pytplot.get_data("brl_"+ss["payload"]+"_RCNT_Interrupt")
-    badirq = rcnt_cnts < 0
+    # Get a list of timestamps for spectra that are contained entirely within the start/end times
+    w = spec.create_timestamp_mask(spect.times, start, end, dur)
+    # combine with goodspect to get a list of all good spectra within the time bounds
+    w = w & goodspect
+    spect_cnts = spect.y[w]
+    spect_time = spect.times[w]
 
+    #Load IRQ data from the RCNT CDF file
+    pyspedas.barrel.rcnt((start, end), ss["payload"])
+    irq = pytplot.get_data("brl"+ss["payload"]+"_Interrupt")
+    goodirq = irq.y >= 0
+
+    #To calculate livetime, use the IRQ rate. Still use "dur" even 
+    #though IRQ is available at 0.25 Hz so that the boundaries match:
+    wl = spec.create_timestamp_mask(irq.times, start, end, dur)
+    # mask out any bad IRQ values
+    wl = wl & goodirq
+    rate_irq = irq.y[wl] / 4 #NOT dur here; units must match
+    irq_time = irq.times[wl]
+
+    #Calculate the rate in the spectrum
+    width, mean, gmean = spec.edge_products(ss["e_bins"])
+    rate_spect = []
+    spectrum = np.zeros(width.size)
+    livetime = 0
+    rawtime = 0
+
+    #sum up, calculating livetime using nearest IRQ measurement to each
+    #spectrum as you go along:
+    #spect.y is /s/keV.
+    for (accum, ts) in zip(spect_cnts, spect_time):
+      rate_spect.append(np.sum(accum*width))
+      dts = np.abs(irq_time-ts)
+      nearest_irq = rate_irq[np.argmin(dts)]
+
+      #set units to counts/keV & we will divide by live seconds later:
+      spectrum += dur*accum/(1. - nearest_irq*8.0e-6)
+      livetime += dur*(1.- nearest_irq*8.0e-6)
+      rawtime += dur
     
+    return (spectrum, livetime, rawtime)
+    
+
+
  
